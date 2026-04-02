9c33879021aa7c6dfe0a02a7b66ac1d904672dd7
Your Name <you@example.com>
2026-04-02 21:38:20 +0530

    added some lessons

--- BACKEND/3.1 Express Server/index.js
+++ BACKEND/3.1 Express Server/index.js
@@ -8,3 +8,11 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
+
+app.get("/contact", (req, res) => {
+    res.send("Contact me at [EMAIL_ADDRESS]");
+});
+
+app.get("/about", (req, res) => {
+    res.send("I am a web developer");
+});
